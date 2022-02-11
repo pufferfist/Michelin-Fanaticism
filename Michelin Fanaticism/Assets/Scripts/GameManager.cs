a65a97faf55b852aff7deee4debb9761c107ab22
Chenxu Bai <bai_chauncey@foxmail.com>
2022-02-11 10:26:30 -0800

    Add different score according to different difficulty

--- Michelin Fanaticism/Assets/Scripts/GameManager.cs
+++ Michelin Fanaticism/Assets/Scripts/GameManager.cs
@@ -21,13 +21,22 @@ public class GameManager : MonoBehaviour
     public Text ingredientText1;
     public Text ingredientText2;
     public Text ingredientText3;
+    public Text ingredientText4;
+    public Text ingredientText5;
+    public Text ingredientText6;
+    public Text ingredientText7;
+    public Text ingredientText8;
+    public Text ingredientText9;
     public Text timeText;
-    public Slider timeSlider;
+    public Slider timeSlider1;
+    public Slider timeSlider2;
+    public Slider timeSlider3;
     public Text currentIngredientText1;
     public Text currentIngredientText2;
     public Text currentIngredientText3;
     private const float EXPIRE_TIME = 30.0f;
-    private List<Menu> menus;
+    private List<Menu> easyMenus;
+    private List<Menu> hardMenus;
     private Menu currentMenu;
     private List<string> pickedIngredients;
     private int currentScore;
@@ -39,19 +48,23 @@ public class GameManager : MonoBehaviour
             gm = GetComponent<GameManager>();
         currentScore = 0;
         gm.gameState = GameState.Playing;
-        timeSlider.maxValue = EXPIRE_TIME;
-        timeSlider.minValue = 0;
-        timeSlider.value = EXPIRE_TIME;
+        timeSlider1.maxValue = EXPIRE_TIME;
+        timeSlider1.minValue = 0;
+        timeSlider1.value = EXPIRE_TIME;
         string[] ingredients1 = {"Beef", "Lettuce", "Bread"};
         string[] ingredients2 = {"Chicken", "Lettuce", "Bread"};
         string[] ingredients3 = {"Bread", "Strawberry"};
+        string[] ingredients4 = {"Bread", "Strawberry","Beef"};
         Menu menu1 = new Menu(1, "Burger", new List<string>(ingredients1));
         Menu menu2 = new Menu(2, "ChickenSandwich", new List<string>(ingredients2));
         Menu menu3 = new Menu(3, "SummerPudding", new List<string>(ingredients3));
-        menus = new List<Menu>();
-        menus.Add(menu1);
-        menus.Add(menu2);
-        menus.Add(menu3);
+        Menu menu4 = new Menu(3, "HealthyFood", new List<string>(ingredients4));
+        easyMenus = new List<Menu>();
+        easyMenus.Add(menu1);
+        easyMenus.Add(menu2);
+        easyMenus.Add(menu3);
+        hardMenus = new List<Menu>();
+        hardMenus.Add(menu4);
         pickedIngredients = new List<string>();
         updateMenu();
         gameStartTime = Time.time;
@@ -89,7 +102,7 @@ public class GameManager : MonoBehaviour
                 }
                 
                 //Update timesLide for the current menu.
-                timeSlider.value = Time.time - currentMenu.startTime;
+                timeSlider1.value = Time.time - currentMenu.startTime;
                 break;
             case GameState.GameOver:
                 Debug.Log("Game is Over");
@@ -119,7 +132,7 @@ public class GameManager : MonoBehaviour
             pickedIngredients.Add(ingredientName);
             if (pickedIngredients.Count == currentMenu.ingredients.Count)
             {
-                addScore();
+                addScore(currentMenu);
                 updateMenu();
             }
             return true;
@@ -127,14 +140,34 @@ public class GameManager : MonoBehaviour
         return false;
     }
 
-    private void addScore()
+    private void addScore(Menu menu)
     {
-        currentScore++;
+        Boolean isFind = false;
+        for (var i = 0; i < easyMenus.Count; i++)
+        {
+            if (menu.name == easyMenus[i].name)
+            {
+                currentScore++;
+                isFind = true;
+                break;
+            }
+        }
+        if (!isFind)
+        {
+            for (var i = 0; i < hardMenus.Count; i++)
+            {
+                if (menu.name == hardMenus[i].name)
+                {
+                    currentScore+=3; 
+                }
+            }
+        }
+
     }
     
     private void updateMenu()
     {
-        timeSlider.value = EXPIRE_TIME;
+        timeSlider1.value = EXPIRE_TIME;
         pickedIngredients = new List<string>();
         currentIngredientText1.text = "";
         currentIngredientText2.text = "";
@@ -142,8 +175,17 @@ public class GameManager : MonoBehaviour
         ingredientText1.text = "";
         ingredientText2.text = "";
         ingredientText3.text = "";
-        int index = Random.Range(0, menus.Count);
-        currentMenu = menus[index];
+        int isHard = Random.Range(0, 2);
+        if (isHard == 1)
+        {
+            int index = Random.Range(0, hardMenus.Count);
+            currentMenu = hardMenus[index];
+        }
+        else
+        {
+            int index = Random.Range(0, easyMenus.Count);
+            currentMenu = easyMenus[index];
+        }
         currentMenu.startTime = Time.time;
         currentMenu.endTime = Time.time + EXPIRE_TIME;
         if (currentMenu.ingredients.Count == 3)
