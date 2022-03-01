73073ba318df976d8b2a052f90138997fd28a7de
JingyanPeng <jingyanp@usc.edu>
2022-02-28 21:52:07 -0800

    Update MainCharacter.cs

--- Michelin Fanaticism/Assets/Scripts/MainCharacter.cs
+++ Michelin Fanaticism/Assets/Scripts/MainCharacter.cs
@@ -24,11 +24,19 @@ public class MainCharacter : MonoBehaviour
         switch (gameState)
         {
             case GameState.Playing:
-                var h = Input.GetAxis("Horizontal");
-                var velocity = rb.velocity;
+                //var h = Input.GetAxis("Horizontal");
+                //var velocity = rb.velocity;
                 var position = rb.position;
-                rb.velocity = new Vector3(velocity.x,velocity.y,-h*speed);
-                rb.position = new Vector3 (position.x, position.y, Mathf.Clamp(position.z ,-14, 14));
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    rb.position = new Vector3(position.x, position.y, Mathf.Clamp(position.z + 7, -7, 7));
+                }
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    rb.position = new Vector3(position.x, position.y, Mathf.Clamp(position.z - 7, -7, 7));
+                }
+                //rb.velocity = new Vector3(velocity.x,velocity.y,-h*speed);
+                //rb.position = new Vector3 (position.x, position.y, Mathf.Clamp(position.z ,-14, 14));
 
                 break;
             default:
