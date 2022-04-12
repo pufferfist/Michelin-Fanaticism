ff4d642c200edcd88650e60f1d474ca8b2e5ead8
Shane Wu <shane@Shanes-MacBook-Pro.local>
2022-04-11 17:57:17 -0700

    guide fix webgl 1 4

--- Michelin Fanaticism/Assets/Scripts/PlayVideo.cs
+++ Michelin Fanaticism/Assets/Scripts/PlayVideo.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using System.IO;
 
+using UnityEngine.Networking;
 public class PlayVideo : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
     private RawImage rawImage;
-
+    public string sourcePath;
     // Use this for initialization
     void Start()
     {
         videoPlayer = this.GetComponent<VideoPlayer>();
         rawImage = this.GetComponent<RawImage>();
+     
+     
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, sourcePath);
+        videoPlayer.url = filePath;
+    
     }
 
     // Update is called once per frame
